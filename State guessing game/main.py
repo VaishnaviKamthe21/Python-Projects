35b57e6c71854617f3ab54d240dadedcfb55c4af
Vaishnavi Kamthe <vaishnavikamthe1884@gmail.com>
2025-03-23 22:13:11 +0530

    added state guess game

---
+++ State guessing game/main.py
@@ -0,0 +1,30 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("States Game")
+political_map = "Python-Projects\State guessing game\political-map-india.gif"
+screen.addshape(political_map)
+turtle.shape(political_map)
+
+states_data = pandas.read_csv("Python-Projects\State guessing game\states.csv" , delimiter='\t')
+states_list = states_data.state.to_list()
+print(states_list)
+score = 0
+
+while len(states_list) > 0:
+    answer = screen.textinput(title=f"Score: {score}/31", prompt="What's the name of the state?")
+    answer=answer.title()
+
+    if answer in states_list:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = states_data[states_data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer)
+        states_list.remove(answer)
+        score += 1
+    
+
+turtle.mainloop()
